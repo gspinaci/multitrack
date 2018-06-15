c1b9cc015b5e39b0dc646e607e96136b490117da
Pasquale Muscettola <pasquale@MacBook-Pro.station>
2018-06-15 19:05:14 +0200

    Fixed syntax error

--- module/leitor_cartao.py
+++ module/leitor_cartao.py
@@ -34,7 +34,7 @@ class LeitorCartao(threading.Thread):
     nfc = Nfc522()
     numero_cartao = None
 
-    def __init__(self, intervalo=0,1):
+    def __init__(self, intervalo=0.1):
         threading.Thread.__init__(self)
         self._stopevent = threading.Event()
         self._sleepperiod = intervalo
