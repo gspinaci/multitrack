2c34960ae836d08de85e2555aa6359d7f00d6ed0
gspinaci <spino9330@gmail.com>
2018-06-20 17:19:05 +0200

    Removed video player

--- base.py
+++ base.py
@@ -5,16 +5,16 @@ from module.leitor_cartao import LeitorCartao
 from module.video_player import VideoPlayerWrapper
 
 reader_card = LeitorCartao()
-video_player_wrapper = VideoPlayerWrapper()
+#video_player_wrapper = VideoPlayerWrapper()
 
 try:
     while True:
         if not reader_card.isAlive():
-            video_player_wrapper.play()
+            #video_player_wrapper.play()
             reader_card.start()
 except KeyboardInterrupt:
     print "trl+C received! Sending kill to " + reader_card.getName()
-    video_player_wrapper.stop()
+    #video_player_wrapper.stop()
     if reader_card.isAlive():
         reader_card._stopevent.set()
 
